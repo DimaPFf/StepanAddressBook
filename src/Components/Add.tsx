9d5297c57170396c19bf19e89fc0f8f427400bba
Dima Petrov <106543728+DimaPFf@users.noreply.github.com>
2023-02-24 08:06:42 +0000

    next step

--- src/Components/Add.tsx
+++ src/Components/Add.tsx
@@ -1,30 +1,39 @@
 import { useState } from "react"
 
-function Add() {
-    const [user, setUser] = useState(
-        {
-            name: '',
-            phone: '',
-            email: ''
-        }
-        )
-    console.log(user);
-    const handleChange = (e:React.SyntheticEvent):void =>{
-        console.log('e',e)
-        setUser(prev=> {
-            return {
-            ...prev,
-            [e.target.name]: e.target.value
-        }
-        })
+type Props = {
+  handleClose: () => void,
+  showModal: boolean
+}
+
+function Add({handleClose}: Props) {
+  const [user, setUser] = useState(
+    {
+      name: '',
+      phone: '',
+      email: ''
     }
+  )
+  console.log(user);
+  const handleChange = (e: React.SyntheticEvent): void => {
+    setUser(prev => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
   return (
-    <form className="add-person">
+    <div className="add-form-modal">
+      <div className="add-form-modal-content">
+      <form className="add-person">
         <input name="name" value={user.name} placeholder="name" onChange={handleChange}></input>
-        <input name="phone" value={user.phone} placeholder="phone"  onChange={handleChange}></input>
-        <input name="email" value={user.email} placeholder="email"  onChange={handleChange}></input>
+        <input name="phone" value={user.phone} placeholder="phone" onChange={handleChange}></input>
+        <input name="email" value={user.email} placeholder="email" onChange={handleChange}></input>
         <button>add</button>
-    </form>
+        <button onClick={handleClose}>X</button>
+      </form>
+        </div>
+    </div>
   )
 }
 
--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { User } from '../type'
 import Add from './Add';
 
@@ -6,49 +6,57 @@ type Props = {
   users: User[]
 }
 
-function Table({users}: Props) {
+function Table({ users }: Props) {
   return (
     <div className="table" >
-        <div className="table--upperRow">
-            <p className="label"> Name </p>
-            <p className="label">Phone </p>
-            <p className="label">Email </p>
-        </div>
-        <Contact users={users} />
+      <div className="table--upperRow">
+        <p className="label"> Name </p>
+        <p className="label">Phone </p>
+        <p className="label">Email </p>
+      </div>
+      <Contact users={users} />
     </div>
   )
 }
 
 export default Table
 
-function Contact({users}: Props){
+function Contact({ users }: Props) {
 
-    const [copied, setCopied] = useState('');
-    const [showModal, setShowModal] = useState(true);
+  const [copied, setCopied] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
-    const copyEmail = async (email: string) =>{
-        try {
-            await navigator.clipboard.writeText(email);
-            setCopied(email);
-          } catch (e) {
-            console.log("error in copy", e);
-          }
+  const copyEmail = async (email: string) => {
+    try {
+      await navigator.clipboard.writeText(email);
+      setCopied(email);
+    } catch (e) {
+      console.log("error in copy", e);
     }
-    
-      return (
-          <div>
-          <ul>
-            {users.map(user => <li id={user.id%2 ? "even" : "odd" } key={user.id} className="container" > 
-            <p className="info">{user.name} </p>
-            <p className="info">{user.phone} </p>
-            <p className="info" id="email" onClick={()=>copyEmail(user.email)}>
-              {user.email} 
-              <img src={copied === user.email ? "tick.png": "copy.png"} className="copy" /> 
-            </p>
-            </li>)} 
-        </ul>
-        <button className='btn-add'>+</button>
-        {showModal && <Add/>}
-        </div>
-      )   
+  }
+
+  const handleAdd = () => {
+    setShowModal(!showModal);
+  }
+
+  const handleClose = () => {
+    setShowModal(!showModal);
+  }
+
+  return (
+    <div>
+      <ul>
+        {users.map(user => <li id={user.id % 2 ? "even" : "odd"} key={user.id} className="container" >
+          <p className="info">{user.name} </p>
+          <p className="info">{user.phone} </p>
+          <p className="info" id="email" onClick={() => copyEmail(user.email)}>
+            {user.email}
+            <img src={copied === user.email ? "tick.png" : "copy.png"} className="copy" />
+          </p>
+        </li>)}
+      </ul>
+      <button onClick={handleAdd} className='btn-add'>+</button>
+      {showModal && <Add handleClose={handleClose} showModal={showModal} />}
+    </div>
+  )
 }
